39c19c2b711eee369de0915dbae5ae42c49f8056
Brenton Currie <brenton.currie@thecloudhub.com>
2019-03-16 07:29:18 +1100

    Initial Commit

---
+++ Digital Ocean Spaces Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Digital Ocean Spaces Example
+//
+//  Created by Brenton on 16/3/19.
+//  Copyright © 2019 The Cloud Hub. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
